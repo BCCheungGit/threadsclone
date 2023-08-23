71edf5e35134e03c9534856591e245cf92d81c6c
BCCheungGit <cheungbenjamin0@gmail.com>
2023-08-23 15:29:26 -0400

    finished user creation and authentication

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -22,6 +22,9 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from 'next/navigation'
+
 
 interface Props {
     user: {
@@ -39,6 +42,9 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const { startUpload } = useUploadThing("media");
     const [files, setFiles] = useState<File[]>([]);
+
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -85,7 +91,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             } 
         }
 
-        // TODO: Update user profile
+        await updateUser({
+          userId: user.id,
+          username: values.username,
+          name: values.name,
+          bio: values.bio,
+          image: values.profile_photo,
+          path: pathname
+        });
+          
+        if (pathname === '/profile/edit') {
+          router.back();
+        } else {
+          router.push('/');
+        }
 
       }
     
