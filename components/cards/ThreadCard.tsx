39e1de73580ecb37358dadd1d349f73ed82b19f0
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-08-31 11:50:20 +0800

    fixed like functionality a bit

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -43,6 +43,7 @@ const ThreadCard = ({
     isComment,
 }: Props) => {
     const [liked, setLiked] = useState(false)
+
     const handleLike = () => {
         setLiked(true)
     }
@@ -79,7 +80,9 @@ const ThreadCard = ({
 
                         <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
                             <div className='flex gap-3.5'>
-                                <Image src={liked ? `/assets/heart-gray.svg` : '/assets/heart-filled.svg'} alt="heart" width={24} height={24} className='cursor-pointer object-contain' onClick={() => {handleLike()}} />
+                                <a onClick={() => {handleLike()}}>
+                                    <Image src={liked ? '/assets/heart-gray.svg' : '/assets/heart-filled.svg'} alt="heart" width={24} height={24} className='cursor-pointer object-contain' />
+                                </a>
                                 <Link href={`/thread/${id}`}>
                                     <Image src="/assets/reply.svg" alt="reply" width={24} height={24} className='cursor-pointer object-contain' />
                                 </Link>
